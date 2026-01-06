a3fd4b93cd88a3a8ffdcdf8acd4546bba777974c
BTM\d-ogata <d-ogata@b-tm.co.jp>
2026-01-06 10:48:03 +0900

    update: sitemap/feat: robots

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,18 +1,25 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://gatsugatsu-gurume.vercel.app";
+  const baseUrl = "https://gatsugatsu-gurume.com";
+  const now = new Date();
 
   return [
     {
       url: baseUrl,
-      lastModified: new Date(),
+      lastModified: now,
       changeFrequency: "daily",
       priority: 1,
     },
     {
+      url: `${baseUrl}/contact`,
+      lastModified: now,
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+    {
       url: `${baseUrl}/privacy`,
-      lastModified: new Date(),
+      lastModified: now,
       changeFrequency: "monthly",
       priority: 0.3,
     },
